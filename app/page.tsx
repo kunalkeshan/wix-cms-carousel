6af5e1e3e9c3d9663d3a0081e94a2517e15e8769
kunalkeshan <kunalkeshan12@gmail.com>
2024-07-06 04:17:58 +0530

    chore: using carousels component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import TestimonialsCarousel from '@/components/testimonials-carousel';
 import { getTestimonials } from '@/lib/wix';
 
 export default async function Home() {
 	const testimonials = await getTestimonials();
-
-	console.log(testimonials);
-	return <main>Page</main>;
+	return (
+		<main>
+			<TestimonialsCarousel testimonials={testimonials} />
+		</main>
+	);
 }
