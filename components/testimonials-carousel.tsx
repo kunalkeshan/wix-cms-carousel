c0233e9be1d2d64509e39322121b91ecf36f4e1a
kunalkeshan <kunalkeshan12@gmail.com>
2024-07-09 14:34:59 +0530

    chore: Refactor testimonials carousel component to handle duplicate testimonials and improve code structure

--- components/testimonials-carousel.tsx
+++ components/testimonials-carousel.tsx
@@ -28,60 +28,62 @@ const TestimonialsCarousel: React.FC<Props> = ({ testimonials }) => {
 			// ]}
 			>
 				<CarouselContent>
-					{testimonials.map((testimonial) => (
-						<CarouselItem
-							key={testimonial._id}
-							className='md:basis-1/2 lg:basis-1/4 h-full'
-						>
-							<div className='text-black relative'>
-								<video
-									className='aspect-[9_/_16] rounded-lg'
-									width='100%'
-									height='570'
-									controls
-									preload='none'
-								>
-									<source
-										src={testimonial.videoUrl}
-										type='video/mp4'
-									/>
-								</video>
-								<div className='absolute bottom-20 left-0 px-4'>
-									<div className='flex gap-2 items-center'>
-										{Array(testimonial.rating)
-											.fill(0)
-											.map((star) => {
-												return (
-													<span key={star}>
-														<StarIcon
-															fill='#facc15'
-															className='text-yellow-400'
-															size={20}
-														/>
-													</span>
-												);
-											})}
-									</div>
-									<div className='text-white pt-2'>
-										<h1 className='text-lg'>
-											{testimonial.title}
-										</h1>
-										<h2 className='text-sm text-slate-300'>
-											{testimonial.designation}
-										</h2>
+					{testimonials
+						.concat(new Array(10).fill(testimonials[0]))
+						.map((testimonial) => (
+							<CarouselItem
+								key={testimonial._id}
+								className='md:basis-1/2 lg:basis-1/4 h-full'
+							>
+								<div className='text-black relative'>
+									<video
+										className='aspect-[9_/_16] rounded-lg'
+										width='100%'
+										height='570'
+										controls
+										preload='none'
+									>
+										<source
+											src={testimonial.videoUrl}
+											type='video/mp4'
+										/>
+									</video>
+									<div className='absolute bottom-20 left-0 px-4'>
+										<div className='flex gap-2 items-center'>
+											{Array(testimonial.rating)
+												.fill(0)
+												.map((star) => {
+													return (
+														<span key={star}>
+															<StarIcon
+																fill='#facc15'
+																className='text-yellow-400'
+																size={20}
+															/>
+														</span>
+													);
+												})}
+										</div>
+										<div className='text-white pt-2'>
+											<h1 className='text-lg'>
+												{testimonial.title}
+											</h1>
+											<h2 className='text-sm text-slate-300'>
+												{testimonial.designation}
+											</h2>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div className='px-4 mt-4'>
-								<p className='text-sm text-justify'>
-									&ldquo;{testimonial.testimonial}&rdquo;
-								</p>
-								<p className='text-xs text-slate-500'>
-									{testimonial.date.toDateString()}
-								</p>
-							</div>
-						</CarouselItem>
-					))}
+								<div className='px-4 mt-4'>
+									<p className='text-sm text-justify'>
+										&ldquo;{testimonial.testimonial}&rdquo;
+									</p>
+									<p className='text-xs text-slate-500'>
+										{testimonial.date.toDateString()}
+									</p>
+								</div>
+							</CarouselItem>
+						))}
 				</CarouselContent>
 			</Carousel>
 		</section>
